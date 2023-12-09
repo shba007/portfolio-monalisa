aa46526fc27c33d7bec67de1a400ae83d06980d3
esconer <hikigaya07@protonmail.com>
2023-12-09 20:10:18 +0530

    style refactor - grid position changed t-css-conf color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,22 +8,22 @@ export default {
       white: "#ffffff",
       light: {
         400: "#F9F8F2",
-        500: "",
+        500: "#D9D9D9",
         600: "#ABABAB",
       },
       black: "black",
       dark: {
-        400: "",
+        400: "#10121E",
       },
       primary: {
         400: "#98DA8B",
         500: "#66CD5B",
-        600: "",
+        600: "#56BE50",
       },
     },
     extend: {
       colors: {
-        mygrey: "#D9D9D9",
+        mygrey: "",
       },
 
       fontFamily: {
