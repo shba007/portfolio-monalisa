4d5dfdf0a429e92da5bfc4ac85f22be0d529ee79
esconer <hikigaya07@protonmail.com>
2023-12-11 04:29:54 +0530

    feat: footer added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("it's working ");
+import Splide from "@splidejs/splide";
+
+new Splide(".splide", {
+  type: "fade",
+  height: "9rem",
+  perPage: 4,
+  breakpoints: {
+    640: {
+      height: "6rem",
+    },
+  },
+}).mount();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    // fontSize: {
+    //   xs: ".75rem",
+    //   sm: "0.875rem",
+    //   md: "1rem",
+    //   lg: "1.25rem",
+    //   xl: "1.5",
+    //   "2xl": "2rem",
+    //   "3xl": "2.5rem",
+    //   "4xl": "3rem",
+    //   "5xl": "3.5rem",
+    // },
     colors: {
       transparent: "transparent",
       current: "currentColor",
