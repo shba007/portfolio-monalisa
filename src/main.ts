070cffba6caab6c7dc38797ce365fa6583281f7b
esconer <hikigaya07@protonmail.com>
2023-12-15 02:01:34 +0530

    feat: testimonials carousal added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import Splide from "@splidejs/splide";
 
-new Splide(".splide", {
-  type: "fade",
-  height: "9rem",
-  perPage: 4,
-  breakpoints: {
-    640: {
-      height: "6rem",
-    },
-  },
-}).mount();
+// Default theme
+import "@splidejs/splide/css";
+
+// or other themes
+import "@splidejs/splide/css/skyblue";
+import "@splidejs/splide/css/sea-green";
+
+// or only core styles
+import "@splidejs/splide/css/core";
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-carousel", {
+    type: "loop",
+    gap: "15rem",
+    focus: "center",
+    autoWidth: true,
+    autoHeight: true,
+  }).mount();
+});
+// document.addEventListener("DOMContentLoaded", function () {
+//   new Splide("#image-carousel").mount();
+// });
