a16ea59889eb666fdb224281c06b1a05e7973529
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-05-23 08:14:22 +0530

    style: appInstallPrompt z index fixed

---
+++ .changeset/soft-mayflies-arrive.md
@@ -0,0 +1,5 @@
+---
+"porfolio-monalisa": patch
+---
+
+style: appInstallPrompt z index fixed
--- components/AppInstallPrompt.vue
+++ components/AppInstallPrompt.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
-    class="fixed bottom-0 left-1/2 -translate-x-1/2" role="alert">
+    class="fixed -bottom-0.5 left-1/2 -translate-x-1/2 z-50" role="alert">
     <button class="pt-2.5 pb-1 px-2 rounded-t-full bg-primary-500 hover:bg-primary-400 transition-colors"
       @click="$pwa?.install()">
       <NuxtIcon name="logo" class="text-[28px] text-dark-400" />
