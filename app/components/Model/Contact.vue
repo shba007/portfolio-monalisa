6901b0a81b245f146e28d9dec2c091cf57ebb250
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-14 00:26:16 +0530

    fix: formatting issue

--- app/components/Model/Contact.vue
+++ app/components/Model/Contact.vue
@@ -48,7 +48,7 @@ function flippedClass(method: string) {
 
 <style scoped>
 a {
-  @apply drop-shadow-[inset_-2px_2px_8px_0_rgba(0, 0, 0, 0.25)] hover:drop-shadow-[inset_-4px_4px_8px_0_rgba(0, 0, 0, 0.25)] bg-primary-500 transition-all hover:bg-primary-400;
+  @apply drop-shadow-[inset_-2px_2px_8px_0_rgba(0,0,0,0.25)] hover:drop-shadow-[inset_-4px_4px_8px_0_rgba(0,0,0,0.25)] bg-primary-500 transition-all hover:bg-primary-400;
   clip-path: polygon(
     100% 100%,
     -0.212% 100%,
